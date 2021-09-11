16521d7586a3ea43c12f2a1ab93ff3f404f43c82
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-09-11 22:15:48 +0300

    Initial Commit

---
+++ CustomShapes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomShapes
+//
+//  Created by Ramill Ibragimov on 11.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomShapes/CustomShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesApp.swift
+//  CustomShapes
+//
+//  Created by Ramill Ibragimov on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
